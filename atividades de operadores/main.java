04cf72bd590ffb0914327a37fd94ff82de59c2df
johnlimaxs <159181135+johnlimaxs@users.noreply.github.com>
2024-04-30 10:47:18 -0300

    Add files via upload

--- atividades de operadores/main.java
+++ atividades de operadores/main.java
@@ -1,19 +1,30 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner leia = new Scanner (System.in);
-
-     int idade = 17;
-
-   System.out.println("Qual sua idade: ");
-     idade = leia.nextInt();
-
-  if (idade >= 18) {
-   System.out.println("Você já pode votar!");
+    
+    public static void main(String[] args){
+        Scanner leia = new Scanner (System.in);
+        int nota = 0;
+        System.out.print("Qual a sua nota?");
+        nota = leia.nextInt();
+        if (nota >= 90 && nota <= 100){
+            System.out.println("Sua nota é " + nota + " e classificação de A");
+        }
+        else if (nota >= 80 && nota <= 89){
+        System.out.println("Sua nota é " + nota + " e classificação de B");
+        }
+        else if (nota >= 70 && nota <= 79){
+            System.out.println("Sua nota é " + nota + " e classificação de C");
+        }
+        else if (nota >= 60 && nota <= 69){
+            System.out.println("Sua nota é "+ nota + " e classificação de D");
+        }
+        else{
+            System.out.println("Sua nota é " + nota + " e classificação de F");
+            }
      }
-     
-  else {
-   System.out.println("Você ainda não pode votar."); }
   }
-}
+    
+    
+ 
+   
